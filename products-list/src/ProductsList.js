a48a82daa855b67563288974fe6f5a9ed1795df9
Stephane Ruhlmann <stephanej.ruhlmann@yahoo.com>
2016-12-24 19:06:51 +0100

    Initial commit

---
+++ products-list/src/ProductsList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './ProductsList.css';
+
+class ProductsList extends Component {
+  render() {
+    return (
+      <div className="ProductsList">
+        <div>Products List</div>
+        // Here goes your products component
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ products-list/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ProductsList from './ProductsList';
+import './index.css';
+
+ReactDOM.render(
+  <ProductsList />,
+  document.getElementById('root')
+);
